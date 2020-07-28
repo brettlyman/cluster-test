967f03e60261fa5c3222119791f63e46da27e0de
Brett Lyman <github@brettlyman.com>
2020-07-27 23:55:32 -0600

    Change name to myapp

--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule Cluster.MixProject do
+defmodule MyApp.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :cluster,
+      app: :myapp,
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
--- test/cluster_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ClusterTest do
-  use ExUnit.Case
-  doctest Cluster
-
-  test "greets the world" do
-    assert Cluster.hello() == :world
-  end
-end
