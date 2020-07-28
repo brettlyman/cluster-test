5f8eb71e6cd7d604b8a7a3b0908f40e5c652c6f9
Brett Lyman <github@brettlyman.com>
2020-07-27 23:58:12 -0600

    cluster supervisor

--- lib/my_app.ex
+++ lib/my_app.ex
@@ -1,18 +1,17 @@
 defmodule MyApp do
-  @moduledoc """
-  Documentation for `MyApp`.
-  """
+  use Application
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MyApp.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start(_type, _args) do
+    topologies = [
+      example: [
+        strategy: Cluster.Strategy.Epmd,
+        config: [hosts: [:"lfx-2@lfx-2", :"lfx-3@lfx-3", :"lfx-4@lfx-4", :"lfx-5@lfx-5"]],
+      ]
+    ]
+    children = [
+      {Cluster.Supervisor, [topologies, [name: MyApp.ClusterSupervisor]]},
+      # ..other children..
+    ]
+    Supervisor.start_link(children, strategy: :one_for_one, name: MyApp.Supervisor)
   end
 end
